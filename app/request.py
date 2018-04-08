f1f87734e0de5972781b4838a807464ee4af904d
zecollokaris <collo.kariss@gmail.com>
2018-04-08 19:16:44 +0300

    end of functionx

--- app/request.py
+++ app/request.py
@@ -7,3 +7,27 @@ News = news.News
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
 
+#Getting the news base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+
+
+def get_newss(category):
+
+    '''Function that gets the json response to our url request'''
+
+    get_newss_url = base_url.format(category,api_key)
+    
+    with urllib.request.urlopen(get_newss_url) as url:
+        get_newss_data = url.read()
+        get_newss_response = json.loads(get_newss_data)
+
+        news_results =None
+
+        if get_newss_response['results']:
+            news_results_list = get_newss_response['results']
+            news_results = process_results(news_results_list)
+
+    return news_results
+
+
