465144931306966c98a79474be737cae462ce6c9
zecollokaris <collo.kariss@gmail.com>
2018-04-08 21:50:13 +0300

    added now showing, and upcoming news

--- app/views.py
+++ app/views.py
@@ -8,9 +8,10 @@ from .request import get_newss
 def index():
 
     '''Veiw root page function that returns the index page and its data'''
-    
+
     #Getting popular News
-    popular_newss = get_newss('popular')
-    print(popular_newss)
+    popular_news = get_newss('popular')
+    upcoming_news = get_newss('upcoming')
+    now_showing_news = get_newss('now_showing')
     title= 'Home -Welcome to the best News Review Website Online'
-    return render_template('index.html',title = title,popular = popular_newss)
+    return render_template('index.html', title = title, popular = popular_news, upcoming = upcoming_news, now_showing = now_showing_news)
