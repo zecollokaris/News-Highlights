17600f78e3dd16a98145ed00460fbe8d1e721bec
zecollokaris <collo.kariss@gmail.com>
2018-04-09 13:27:12 +0300

    updated route

--- app/views.py
+++ app/views.py
@@ -6,3 +6,13 @@ from .request import get_newss
 from .request import get_newss,get_news
 
 
+#Views
+@app.route('/news/<int:id>')
+def news():
+
+    '''Veiw news page function that returns the news details page and its data'''
+
+    # Getting popular News
+    news = get_news(id)
+    title= f'{news.title}'
+    return render_template('news.html',title = title,news = news)
